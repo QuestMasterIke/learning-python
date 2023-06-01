25dcde2f53a53c165174709cd2f4ce1234d91337
QuestMasterIke <nicolai.scheerer@gmail.com>
2023-06-01 11:25:24 +0200

    Update helloworld_start.py
    Erste Übung

--- Ch2 - Basics/helloworld_start.py
+++ Ch2 - Basics/helloworld_start.py
@@ -3,4 +3,12 @@
 # LinkedIn Learning Python course by Joe Marini
 #
 
+def main():
+    print("Hello World")
+    name = input("Wie ist dein Name?")
 
+    print("Schön dich kennenzulernen,",name)
+  
+
+if __name__ == "__main__":    
+    main()
